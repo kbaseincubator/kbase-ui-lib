8af0d3bc86e146bc22ea355d39996fe7daf778d6
Erik Pearson <eapearson@lbl.gov>
2019-06-25 08:15:02 -0700

    try adding fonts

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,3 +1,4 @@
 rm -rf ./lib/*
 tsc
 copyfiles -u 1  "./src/**/*.css" "./lib/"
+copyfiles -u 1 "./src/style/**/*" "./lib/"
--- src/components/AppBase.tsx
+++ src/components/AppBase.tsx
@@ -1,42 +1,43 @@
 /**
  * A base react component wraps the kbase-ui integration components.
  */
-import * as React from "react";
-import "./AppBase.css";
-import KBaseIntegrationLoader from "./integration/loader";
-import KBaseAuthLoader from "./auth/loader";
-import "antd/dist/antd.css";
-import "typeface-oxygen";
+import * as React from 'react';
+import './AppBase.css';
+import '../style/fonts.css';
+import KBaseIntegrationLoader from './integration/loader';
+import KBaseAuthLoader from './auth/loader';
+import 'antd/dist/antd.css';
+// import "typeface-oxygen";
 
 export interface AppProps {}
 
 interface AppState {
-  clicks: number;
+    clicks: number;
 }
 
 export default class AppBase extends React.Component<AppProps, AppState> {
-  hosted: boolean;
-  constructor(props: AppProps) {
-    super(props);
-    this.hosted = window.frameElement ? true : false;
-    this.state = {
-      clicks: 0
-    };
-  }
+    hosted: boolean;
+    constructor(props: AppProps) {
+        super(props);
+        this.hosted = window.frameElement ? true : false;
+        this.state = {
+            clicks: 0
+        };
+    }
 
-  clickMe() {
-    this.setState({ clicks: this.state.clicks + 1 });
-  }
+    clickMe() {
+        this.setState({ clicks: this.state.clicks + 1 });
+    }
 
-  render() {
-    return (
-      <KBaseIntegrationLoader>
-        <KBaseAuthLoader hosted={this.hosted}>
-          <div className="Row Row-fullheight Row-scrollable" data-k-b-testhook-component="AppBase">
-            {this.props.children}
-          </div>
-        </KBaseAuthLoader>
-      </KBaseIntegrationLoader>
-    );
-  }
+    render() {
+        return (
+            <KBaseIntegrationLoader>
+                <KBaseAuthLoader hosted={this.hosted}>
+                    <div className="Row Row-fullheight Row-scrollable" data-k-b-testhook-component="AppBase">
+                        {this.props.children}
+                    </div>
+                </KBaseAuthLoader>
+            </KBaseIntegrationLoader>
+        );
+    }
 }
