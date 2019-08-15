1be74f4306ec3cce28ff8d759eed21df0b15db4c
Erik Pearson <eapearson@lbl.gov>
2019-08-14 18:38:27 -0700

    expose navigate action in exports

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,6 @@ export { default as baseReducer } from './redux/reducers';
 export { AppError, BaseStoreState, makeBaseStoreState } from './redux/store';
 export { default as NiceTimeDuration } from './components/NiceTimeDuration';
 export { default as NiceRelativeTime } from './components/NiceRelativeTime';
-export { sendTitle, setTitle } from './redux/integration/actions';
+export { sendTitle, setTitle, navigate } from './redux/integration/actions';
 export { default as AuthGate } from './components/AuthGate';
 export { setView, setParams } from './redux/develop/actions';
