59dac6a4056f8f731e3baa17bc36cf5d1d29c5f2
Erik Pearson <eapearson@lbl.gov>
2019-08-23 14:32:51 -0700

    add antd custom less

--- scripts/build.bash
+++ scripts/build.bash
@@ -4,4 +4,5 @@ rm -rf ./templib
 copyfiles -u 1  "./build/**/*" "./templib/"
 copyfiles -u 1  "./src/**/*.css" "./templib/"
 copyfiles -u 1 "./src/style/**/*" "./templib/"
+copyfiles -u 1 "./src/custom/**/*" "./templib/"
 mv ./templib ./lib
