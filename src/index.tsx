82385a2b8bcf2284afb4e9bf5d7a19d2bfcc442b
Erik Pearson <eapearson@lbl.gov>
2019-09-05 04:45:03 -0700

    expose AuthClient

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,5 @@ export { ServiceClient, AuthorizedServiceClient } from './lib/comm/ServiceClient
 export { DynamicServiceClient, DynamicServiceClientParams } from './lib/comm/DynamicServiceClient';
 export { default as NarrativeJobServiceClient } from './lib/coreServices/NarrativeJobService';
 export { default as CatalogClient } from './lib/coreServices/Catalog';
+export { default as AuthClient } from './lib/coreServices/auth';
 export { niceDuration, niceElapsed, niceRelativeTime, NiceRelativeTimeOptions } from './lib/time'
