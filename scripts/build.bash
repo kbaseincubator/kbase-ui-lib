2f7c156c636c338bd41d96b0e6e69139f477eabe
Erik Pearson <eapearson@lbl.gov>
2023-02-05 09:38:09 -0800

    simplify build and fix source maps [UFI-33]

--- scripts/build.bash
+++
@@ -1,6 +0,0 @@
-rm -rf ./lib
-tsc
-copyfiles -u 1  "./build/**/*" "./lib/"
-copyfiles -u 1  "./src/**/*.css" "./lib/"
-copyfiles -u 1 "./src/style/**/*" "./lib/"
-copyfiles -u 1 "./src/custom/**/*" "./lib/"
